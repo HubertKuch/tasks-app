43f1d33a3c1b8957762800a1ac61cc2afaf6e65a
Hubert Kuch <kuchhubert@gmail.com>
2025-08-12 11:33:59 +0200

    feat: login form

---
+++ app/Livewire/LoginView.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LoginView extends Component
+{
+    public function render()
+    {
+        return view('views.login-view');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Livewire\LoginView;
 use App\Livewire\MainView;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', MainView::class);
+Route::get('/login', LoginView::class);
